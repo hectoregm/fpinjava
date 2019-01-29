31ad34d1494fdc149173990605ed38b7ad9218ed
Hector E. Gomez Morales <hectoregm@gmail.com>
2019-01-28 20:01:38 -0800

    Finish chapter 2.

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
@@ -6,17 +6,22 @@ public class FunctionExamples {
    * Note: The same implementation may be used for all four cases.
    */
 
-  public static final Function<Integer, Integer> factorial0 = null; // To be implemented
+  public static final Function<Integer, Integer> factorial0 = new Function<Integer, Integer>() {
+    @Override
+    public Integer apply(Integer n) {
+      return n == 0 ? 1 : n * apply(n - 1);
+    }
+  }; // To be implemented
 
   public static Function<Integer, Integer> factorial1;
   static {
-    factorial1 = null; // To be implemented
+    factorial1 =  n -> n <= 1 ? n : n * factorial1.apply(n - 1);
   }
 
-  public final Function<Integer, Integer> factorial2 = null; // To be implemented
+  public final Function<Integer, Integer> factorial2 = n -> n <= 1 ? n : n * this.factorial2.apply(n - 1);
 
   public Function<Integer, Integer> factorial3;
   {
-    factorial3 = null; // To be implemented
+    factorial3 = n -> n <= 1 ? n : n * factorial3.apply(n - 1);
   }
 }
